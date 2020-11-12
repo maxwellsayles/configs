d8538fe7e38290fd46b822f940072a191bb56de7
Maxwell Sayles <maxwellsayles@gmail.com>
2020-11-11 17:25:16 -0800

    sync with work setup

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -1,7 +1,8 @@
 power_status=$(cat /sys/class/power_supply/BAT0/status)
 power_percent=$(cat /sys/class/power_supply/BAT0/capacity)
 power=$(echo ${power_percent}% $power_status)
-now=$(date +'%Y-%m-%d %l:%M %p')
+date=$(date +'%Y-%m-%d')
+time=$(date +'%l:%M %p')
 
 # Emojis https://unicode.org/emoji/charts/full-emoji-list.html
-echo "🔋 $power  🧭 $now  "
+echo "🔋 $power  📅 $date  🧭 $time  "
